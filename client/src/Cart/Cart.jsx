77d97d9ac6930cd07b182425feb4962e2ae170ac
Ton Nguyen <104323824+tonnguyen123@users.noreply.github.com>
2025-03-28 14:54:08 -0500

    Update Cart.jsx

--- client/src/Cart/Cart.jsx
+++ client/src/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import './cart.css';
 
 export const Cart = () => {
